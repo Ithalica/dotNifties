b002f504ca6d8746c9f930c90142e0171b910b8e
Steffen Dam <ithalica@gmail.com>
2016-09-29 00:05:30 +0200

    Added WhereNotNull method and docs

--- dotNifties/dotNifties/Extensions/IEnumerableExtensions.cs
+++ dotNifties/dotNifties/Extensions/IEnumerableExtensions.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dotNifties.Extensions
 {
     public static class IEnumerableExtensions
     {
-
+        /// <summary>
+        /// Shuffles the elements of a sequence
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An System.Collections.Generic.IEnumerable`1 to shuffle</param>
+        /// <returns>An System.Collections.Generic.IEnumerable`1 that contains the reshuffled elements of source</returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
             Random rand = new Random();
             return source.OrderBy(_ => rand.Next());
         }
-        
+
+        /// <summary>
+        /// Swaps places of two elements in a sequence
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An System.Collections.Generic.IEnumerable`1 to perform the swap on</param>
+        /// <param name="firstIndex">The first index to be swapped with the second</param>
+        /// <param name="secondIndex">The second index to be swapped with the first</param>
+        /// <returns>An System.Collections.Generic.IEnumerable`1 where firstIndex and secondIndex have been swapped</returns>
         public static IEnumerable<T> Swap<T>(this IEnumerable<T> source, int firstIndex, int secondIndex)
         {
             var arr = source.ToArray();
@@ -22,6 +33,17 @@ namespace dotNifties.Extensions
             arr[firstIndex] = arr[secondIndex];
             arr[secondIndex] = tempItem;
             return source;
+        }
+
+        /// <summary>
+        /// A semantically better way to remove null-values from a sequence
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An System.Collections.Generic.IEnumerable`1 to remove NULL-value elements on</param>
+        /// <returns>An System.Collections.Generic.IEnumerable`1 where elements with the value of NULL have been filtered out</returns>
+        public static IEnumerable<T> WhereNotNull<T> (this IEnumerable<T> source)
+        {
+            return source.Where(x => x != null);
         }
     }
 }
