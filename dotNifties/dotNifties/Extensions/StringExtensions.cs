6b29c706e10fe3e7e354a01b9c9b1ae3ae100ae0
Steffen Dam <ithalica@gmail.com>
2016-09-29 00:06:05 +0200

    Added IsNullOrWhiteSpace method and docs

--- dotNifties/dotNifties/Extensions/StringExtensions.cs
+++ dotNifties/dotNifties/Extensions/StringExtensions.cs
@@ -8,12 +8,29 @@ namespace dotNifties.Extensions
 {
    public static class StringExtensions
     {
+        /// <summary>
+        /// Check for equality on two strings, without regard for leading or trailing whitespaces or case.
+        /// </summary>
+        /// <param name="source">String to test</param>
+        /// <param name="target">String to test against</param>
+        /// <returns>true if the value parameter is equal to the target, ignoring leading and trailing whitespaces and case</returns>
         public static bool EqualsTrimmedInvariant(this string source, string target)
         {
             string str1 = (source ?? "").ToLowerInvariant().Trim();
             string str2 = (target ?? "").ToLowerInvariant().Trim();
 
             return str1.Equals(str2);
+        }
+
+        /// <summary>
+        /// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+        /// Wrapper of string.IsNullOrWhiteSpoace(string)
+        /// </summary>
+        /// <param name="source">The string to test</param>
+        /// <returns>true if the value parameter is null or System.String.Empty, or if value consists exclusively of white-space characters</returns>
+        public static bool IsNullOrWhiteSpace(this string source)
+        {
+            return string.IsNullOrWhiteSpace(source);
         }
     }
 }
